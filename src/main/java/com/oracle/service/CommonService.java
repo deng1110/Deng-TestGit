ea77ae30383d6153528afa93a784d0a59a1e348e
deng <1234>
2018-05-21 15:43:34 +0800

    service first

---
+++ src/main/java/com/oracle/service/CommonService.java
@@ -0,0 +1,10 @@
+package com.oracle.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommonService {
+    public void sqy(){
+        System.out.println("这是一个CommonService");
+    }
+}
